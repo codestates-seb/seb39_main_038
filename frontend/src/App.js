dac049efd48dc409853c859e6177b6b8ec0aa659
YHJ96 <yhj878812@naver.com>
2022-09-07 19:21:34 +0900

    feat: 프론트엔드 기본 세팅 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  background-color: blue;
+  width: 300;
+  height: 300;
+`;
 
 function App() {
-  return <div style={{ width: 300, height: 300, background: 'red' }}>App</div>;
+  return <Div />;
 }
 
 export default App;
