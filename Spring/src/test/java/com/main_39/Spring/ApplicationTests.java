233bc5b266e065585645a8724d84461ab1a6173c
ds02168 <1995musso@gmail.com>
2022-09-08 17:07:02 +0900

    feat: 백엔드 초기설정

---
+++ Spring/src/test/java/com/main_39/Spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main_39.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
