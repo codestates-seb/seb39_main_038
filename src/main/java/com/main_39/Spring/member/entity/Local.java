0a9e706163317ffd1c645758aea8721f43063fd1
ds02168 <1995musso@gmail.com>
2022-09-13 18:47:01 +0900

    feat: CSRF, CORS 설정 허용

---
+++ src/main/java/com/main_39/Spring/member/entity/Local.java
@@ -0,0 +1,4 @@
+package com.main_39.Spring.member.entity;
+
+public class Local {
+}
---
+++ src/main/java/com/main_39/Spring/member/service/MemberService.java
@@ -0,0 +1,7 @@
+package com.main_39.Spring.member.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+}
