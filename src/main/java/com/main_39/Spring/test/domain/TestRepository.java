ff2789e00ba4df9fcc54e2b008cbc002118c86f1
lkc9898 <104333737+lkc9898@users.noreply.github.com>
2022-09-16 16:58:23 +0900

    feat: api 테스트 도메인

---
+++ src/main/java/com/main_39/Spring/test/domain/TestRepository.java
@@ -0,0 +1,6 @@
+package com.main_39.Spring.test.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository extends JpaRepository<Test, Long> {
+}
---
+++ src/main/java/com/main_39/Spring/test/dto/PostRequestDto.java
@@ -0,0 +1,9 @@
+package com.main_39.Spring.test.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostRequestDto {
+    private String name;
+    private String phone;
+}
