9a1b82d87921d68581b4ea0e074d98785113e855
YHJ96 <yhj878812@naver.com>
2022-09-19 16:54:59 +0900

    feat: IDINQUIRY & PWINQUIRY 프로퍼티 추가 완료

--- frontend/src/constants/route.js
+++ frontend/src/constants/route.js
@@ -2,8 +2,8 @@ const ROUTE = {
   HOME: { PATH: '/' },
   LOGIN: { PATH: 'login', NAME: '로그인' },
   REGISTER: { PATH: 'register' },
-  IDINQUIRY: { PATH: 'idinquiry' },
-  PWINQUIRY: { PATH: 'pwinquiry' },
+  IDINQUIRY: { PATH: 'idinquiry', NAME: '아이디 찾기' },
+  PWINQUIRY: { PATH: 'pwinquiry', NAME: '비밀번호 찾기' },
   FOODLIST: { PATH: 'foodlist' },
   BASKET: { PATH: 'BASKET', NAME: '장바구니' },
 };
