b4277331f7c1dd7f0c51638afdb177bae47ba322
YHJ96 <yhj878812@naver.com>
2022-09-19 13:53:19 +0900

    feat: 카카오 Oauth 2.0 로그인 구현 로직 완료

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { LoginContainer, LoginWrapper, Input, Button } from './styles';
+import { KAKAO_API, ROUTE } from '../../constants';
 
 function Login() {
+  const hanldeOnSumbmit = (e) => e.preventDefault();
+  const handleOnClick = (api) => () => window.location.assign(api);
+
   return (
     <LoginContainer>
-      <LoginWrapper>
+      <LoginWrapper onSubmit={hanldeOnSumbmit}>
         <Input placeholder="이메일 주소 입력" />
         <Input placeholder="비밀번호 입력" />
-        <Button>로그인</Button>
-        <Button>카카오 로그인</Button>
+        <Button>{ROUTE.LOGIN.NAME}</Button>
+        <Button onClick={handleOnClick(KAKAO_API.URI())}>
+          {KAKAO_API.NAME}
+        </Button>
       </LoginWrapper>
     </LoginContainer>
   );
