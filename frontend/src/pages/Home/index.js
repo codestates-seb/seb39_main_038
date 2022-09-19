9435af308e8d8b03386c74004a7f7cd44758438f
YHJ96 <yhj878812@naver.com>
2022-09-19 13:50:49 +0900

    feat: 카카오 Oauth 2.0 로그인 구현 로직 완료

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function Home() {
+  const postAuthData = (api, data) => {
+    if (api === null) return;
+    axios
+      .post(api, data)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err.message));
+  };
+
+  useEffect(() => {
+    const redirectURI = new URL(window.location.href);
+    const code = redirectURI.searchParams.get('code');
+    if (!code) return;
+    postAuthData(null, { code });
+  }, []);
+
   return <div>Home</div>;
 }
 
