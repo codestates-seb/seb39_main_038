d0f4b3b23d62d6c0ad66af93732aed3067fb8058
YHJ96 <yhj878812@naver.com>
2022-09-19 16:56:37 +0900

    feat: API_URI 프로퍼티 추가 완료

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,6 +1,6 @@
 import { ROUTE } from './route';
 import { COLOR } from './color';
 import { SCREEN } from './screen';
-import { KAKAO_API } from './api';
+import { KAKAO_API, API_URI } from './api';
 
-export { ROUTE, COLOR, SCREEN, KAKAO_API };
+export { ROUTE, COLOR, SCREEN, KAKAO_API, API_URI };
