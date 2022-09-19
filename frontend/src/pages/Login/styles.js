c723f546655fbf008c0c6c19b97b982b8ade5d64
YHJ96 <yhj878812@naver.com>
2022-09-19 16:54:22 +0900

    design: Login 컴포넌트 UI & CSS 구현 완료

--- frontend/src/pages/Login/styles.js
+++ frontend/src/pages/Login/styles.js
@@ -3,12 +3,49 @@ import styled from 'styled-components';
 const LoginContainer = styled.div`
   max-width: 800px;
   margin: 0 auto;
+  height: calc(100vh - 80px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Logo = styled.div`
+  height: 45px;
+  background-color: red;
 `;
 
 const LoginWrapper = styled.form`
   display: flex;
   flex-direction: column;
   padding: 20px 0px;
+`;
+
+const LoginInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const FindInner = styled.span`
+  display: flex;
+  justify-content: flex-end;
+  padding: 20px 0px;
+  a:first-child::after {
+    content: '|';
+    line-height: 1.4;
+    padding-left: 4px;
+  }
+  a:last-child {
+    padding-left: 4px;
+    line-height: 1.4;
+  }
+`;
+
+const FindText = styled.a`
+  text-decoration: none;
+  font-size: 14px;
+  color: #999999;
+  cursor: pointer;
 `;
 
 const Input = styled.input`
@@ -19,11 +56,20 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-  font-size: 14px;
+  font-size: 15px;
   padding: 10px 15px;
   border: 1px solid #ddd;
   border-radius: 2px;
   background-color: transparent;
 `;
 
-export { LoginContainer, LoginWrapper, Input, Button };
+export {
+  LoginContainer,
+  Logo,
+  LoginWrapper,
+  LoginInner,
+  FindInner,
+  FindText,
+  Input,
+  Button,
+};
