d7c499c3b55850ba3023e2aedc9154d6219295e4
YHJ96 <yhj878812@naver.com>
2022-09-19 13:54:25 +0900

    feat: api 추가 완료

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,5 +1,6 @@
 import { ROUTE } from './route';
 import { COLOR } from './color';
 import { SCREEN } from './screen';
+import { KAKAO_API } from './api';
 
-export { ROUTE, COLOR, SCREEN };
+export { ROUTE, COLOR, SCREEN, KAKAO_API };
