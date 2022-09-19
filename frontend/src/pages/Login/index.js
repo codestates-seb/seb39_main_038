404494902e5e9c579fb2ef5908f0e1b6c346a14f
YHJ96 <yhj878812@naver.com>
2022-09-19 12:25:41 +0900

    feat: Login 컴포넌트 와이어프레임 구현 완료

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { LoginContainer, LoginWrapper, Input, Button } from './styles';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <LoginContainer>
+      <LoginWrapper>
+        <Input placeholder="이메일 주소 입력" />
+        <Input placeholder="비밀번호 입력" />
+        <Button>로그인</Button>
+        <Button>카카오 로그인</Button>
+      </LoginWrapper>
+    </LoginContainer>
+  );
 }
 
 export default Login;
