86ca0a3d5a9f59f72436c938466f0a8a78ec3f58
YHJ96 <yhj878812@naver.com>
2022-09-21 01:13:12 +0900

    refactor: 유동 데이터 UI 랜더링 수정 완료

--- frontend/src/components/LocalNav/index.js
+++ frontend/src/components/LocalNav/index.js
@@ -1,32 +1,18 @@
 import React from 'react';
 import { NavContainer, NavWrapper, NavList, Navitem } from './styles';
+import { MENU } from '../../constants';
 
 function LocalNav() {
+  const createNavList = () => {
+    return MENU.map((item) => (
+      <NavList id={item.id}>
+        <Navitem>{item.title}</Navitem>
+      </NavList>
+    ));
+  };
   return (
     <NavContainer>
-      <NavWrapper>
-        <NavList>
-          <Navitem>전체보기</Navitem>
-        </NavList>
-        <NavList>
-          <Navitem>양식</Navitem>
-        </NavList>
-        <NavList>
-          <Navitem>한식</Navitem>
-        </NavList>
-        <NavList>
-          <Navitem>중식</Navitem>
-        </NavList>
-        <NavList>
-          <Navitem>분식</Navitem>
-        </NavList>
-        <NavList>
-          <Navitem>야식</Navitem>
-        </NavList>
-        <NavList>
-          <Navitem>카페/디저트</Navitem>
-        </NavList>
-      </NavWrapper>
+      <NavWrapper>{createNavList()}</NavWrapper>
     </NavContainer>
   );
 }
