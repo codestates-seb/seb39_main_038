e521e36aacf130b5fcbaf13bbb78ed5c1605eee4
YHJ96 <yhj878812@naver.com>
2022-09-21 00:55:58 +0900

    feat: location 데이터 정제 완료

--- frontend/src/pages/FoodList/index.js
+++ frontend/src/pages/FoodList/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import FoodDetail from './FoodDetail';
 
 function FoodList() {
+  const location = useLocation();
+  console.log(location.state.query);
+
   return (
     <div>
       FoodList <FoodDetail />
