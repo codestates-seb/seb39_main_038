388683adc776e85f5a18586c4499dc9d8ce47fe7
YHJ96 <yhj878812@naver.com>
2022-09-20 17:43:15 +0900

    feat: REGISTER 프로퍼티 추가 완료

--- frontend/src/constants/route.js
+++ frontend/src/constants/route.js
@@ -1,11 +1,11 @@
 const ROUTE = {
   HOME: { PATH: '/' },
+  FOODLIST: { PATH: 'foodlist' },
   LOGIN: { PATH: 'login', NAME: '로그인' },
-  REGISTER: { PATH: 'register' },
+  REGISTER: { PATH: 'register', NAME: '회원가입' },
   IDINQUIRY: { PATH: 'idinquiry', NAME: '아이디 찾기' },
   PWINQUIRY: { PATH: 'pwinquiry', NAME: '비밀번호 찾기' },
-  FOODLIST: { PATH: 'foodlist' },
-  BASKET: { PATH: 'BASKET', NAME: '장바구니' },
+  BASKET: { PATH: 'basket', NAME: '장바구니' },
 };
 
 export { ROUTE };
