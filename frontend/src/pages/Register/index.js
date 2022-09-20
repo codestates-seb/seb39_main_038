a6f50811c3670e6990222b76308df28328088c61
YHJ96 <yhj878812@naver.com>
2022-09-20 17:56:05 +0900

    design: Register 와이어프레임 구현 완료

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {
+  FormContainer,
+  FormWrapper,
+  FormInput,
+  FormButton,
+} from '../../components/Form';
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <FormContainer>
+      <FormWrapper>
+        <FormInput placeholder="이메일 주소 입력" />
+        <FormInput placeholder="이름 입력" />
+        <FormInput placeholder="휴대폰 전화번호 입력 (-제외)" />
+        <FormInput placeholder="비밀번호 입력" />
+        <FormInput placeholder="비밀번호 확인" />
+        <FormButton>회원가입</FormButton>
+      </FormWrapper>
+    </FormContainer>
+  );
 }
 
 export default Register;
