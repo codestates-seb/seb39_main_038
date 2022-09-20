7d9ad79a3962b5d12aeec78d53ce1299b994c5f5
Mark1237200 <0915kimjw@gmail.com>
2022-09-21 02:14:53 +0900

    Design: FoodDetail 클린리뷰 CSS 구성 완료

--- frontend/src/components/DetailMenuBar/index.js
+++ frontend/src/components/DetailMenuBar/index.js
@@ -1,28 +1,193 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const MenuBar = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  margin: 20px 0px 0px;
-`;
-
-const TabBtn = styled.button`
-  width: 33.3%;
-  height: 47px;
-  background-color: ${(e) => e.theme.mainColor};
-  color: ${(e) => e.theme.fontColor};
-  font-size: 20px;
-  font-weight: 900;
-`;
+import { useRecoilState } from 'recoil';
+import { menuTab } from './atom';
+import {
+  MenuBar,
+  TabBtn,
+  Section,
+  Menu,
+  MenuInfo,
+  MenuImg,
+  Rating,
+  Comment,
+  NameDateReply,
+  ReplyDeleteBtn,
+  ThumnailBox,
+  Thumnail,
+} from './styles';
 
 function DetailMenuBar() {
+  const [menu, setMenu] = useRecoilState(menuTab);
+
   return (
-    <MenuBar>
-      <TabBtn type="button">메뉴 35</TabBtn>
-      <TabBtn type="button">클린리뷰 1740</TabBtn>
-      <TabBtn type="button">정보</TabBtn>
-    </MenuBar>
+    <Section>
+      <MenuBar>
+        <TabBtn
+          type="button"
+          onClick={() => {
+            setMenu('메뉴');
+          }}
+        >
+          메뉴 35
+        </TabBtn>
+        <TabBtn
+          type="button"
+          onClick={() => {
+            setMenu('리뷰');
+          }}
+        >
+          클린리뷰 1740
+        </TabBtn>
+        <TabBtn
+          type="button"
+          onClick={() => {
+            setMenu('정보');
+          }}
+        >
+          정보
+        </TabBtn>
+      </MenuBar>
+      {menu === '메뉴' ? <DetailFoodMenu /> : null}
+      {menu === '리뷰' ? <DetailReview /> : null}
+      {menu === '정보' ? '정보' : null}
+    </Section>
+  );
+}
+
+function DetailFoodMenu() {
+  return (
+    <Section>
+      <Menu>
+        <MenuInfo>
+          <span>치킨</span>
+          <span>빠삭한 치킨입니다!</span>
+          <span>12,000원</span>
+        </MenuInfo>
+
+        <MenuImg>
+          <img alt="thumb" />
+        </MenuImg>
+      </Menu>
+
+      <Menu>
+        <MenuInfo>
+          <span>치킨</span>
+          <span>빠삭한 치킨입니다!</span>
+          <span>12,000원</span>
+        </MenuInfo>
+
+        <MenuImg>
+          <img alt="thumb" />
+        </MenuImg>
+      </Menu>
+    </Section>
+  );
+}
+
+function DetailReview() {
+  return (
+    <Section>
+      <Rating>5.0 ★★★★★</Rating>
+      <Comment>
+        <NameDateReply>
+          <span>김재원 2022-09-20</span>
+          <div>
+            <ReplyDeleteBtn type="button">답글</ReplyDeleteBtn>
+            <ReplyDeleteBtn type="button">삭제</ReplyDeleteBtn>
+          </div>
+        </NameDateReply>
+        <div>별점 ★★</div>
+        <ThumnailBox>
+          <Thumnail alt="Thumnail" />
+        </ThumnailBox>
+        <div>주문내역</div>
+        <div>
+          사장님도 친절하시고, 양도 많고 엄청 신선했습니다! 많이 시켜먹을 것
+          같습니다!
+        </div>
+      </Comment>
+      <Comment>
+        <NameDateReply>
+          <span>김재원 2022-09-21</span>
+          <div>
+            <ReplyDeleteBtn type="button">답글</ReplyDeleteBtn>
+            <ReplyDeleteBtn type="button">삭제</ReplyDeleteBtn>
+          </div>
+        </NameDateReply>
+        <div>별점 ★★★★</div>
+        <ThumnailBox>
+          <Thumnail alt="Thumnail" />
+        </ThumnailBox>
+        <div>주문내역</div>
+        <div>맛은 없는거 같아요...</div>
+      </Comment>
+      <Comment>
+        <NameDateReply>
+          <span>김재원 2022-09-20</span>
+          <div>
+            <ReplyDeleteBtn type="button">답글</ReplyDeleteBtn>
+            <ReplyDeleteBtn type="button">삭제</ReplyDeleteBtn>
+          </div>
+        </NameDateReply>
+        <div>별점 ★★</div>
+        <ThumnailBox>
+          <Thumnail alt="Thumnail" />
+        </ThumnailBox>
+        <div>주문내역</div>
+        <div>
+          사장님도 친절하시고, 양도 많고 엄청 신선했습니다! 많이 시켜먹을 것
+          같습니다!
+        </div>
+      </Comment>
+      <Comment>
+        <NameDateReply>
+          <span>김재원 2022-09-21</span>
+          <div>
+            <ReplyDeleteBtn type="button">답글</ReplyDeleteBtn>
+            <ReplyDeleteBtn type="button">삭제</ReplyDeleteBtn>
+          </div>
+        </NameDateReply>
+        <div>별점 ★★★★</div>
+        <ThumnailBox>
+          <Thumnail alt="Thumnail" />
+        </ThumnailBox>
+        <div>주문내역</div>
+        <div>맛은 없는거 같아요...</div>
+      </Comment>
+      <Comment>
+        <NameDateReply>
+          <span>김재원 2022-09-20</span>
+          <div>
+            <ReplyDeleteBtn type="button">답글</ReplyDeleteBtn>
+            <ReplyDeleteBtn type="button">삭제</ReplyDeleteBtn>
+          </div>
+        </NameDateReply>
+        <div>별점 ★★</div>
+        <ThumnailBox>
+          <Thumnail alt="Thumnail" />
+        </ThumnailBox>
+        <div>주문내역</div>
+        <div>
+          사장님도 친절하시고, 양도 많고 엄청 신선했습니다! 많이 시켜먹을 것
+          같습니다!
+        </div>
+      </Comment>
+      <Comment>
+        <NameDateReply>
+          <span>김재원 2022-09-21</span>
+          <div>
+            <ReplyDeleteBtn type="button">답글</ReplyDeleteBtn>
+            <ReplyDeleteBtn type="button">삭제</ReplyDeleteBtn>
+          </div>
+        </NameDateReply>
+        <div>별점 ★★★★</div>
+        <ThumnailBox>
+          <Thumnail alt="Thumnail" />
+        </ThumnailBox>
+        <div>주문내역</div>
+        <div>맛은 없는거 같아요..</div>
+      </Comment>
+    </Section>
   );
 }
 
--- frontend/src/style/theme.js
+++ frontend/src/style/theme.js
@@ -8,9 +8,9 @@ const theme = {
   navColor: '182061',
   mainColor: '#16267D',
   fontColor: 'F4B504',
-  mobile: `screen and (max-width: ${size.mobile})`,
-  tablet: `screen and (max-width: ${size.tablet})`,
-  desktop: `screen and (max-width: ${size.desktop})`,
+  mobile: `screen and (min-width: ${size.mobile})`,
+  tablet: `screen and (min-width: ${size.tablet})`,
+  desktop: `screen and (min-width: ${size.desktop})`,
 };
 
 export default theme;
