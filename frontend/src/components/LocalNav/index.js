ca6017a9d9bc22620ad359348f5e90f6a5f33d09
YHJ96 <yhj878812@naver.com>
2022-09-20 22:27:37 +0900

    design: LocalNav 와이어프레임 구현 완료

--- frontend/src/components/LocalNav/index.js
+++ frontend/src/components/LocalNav/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { NavContainer, NavWrapper, NavList, Navitem } from './styles';
 
 function LocalNav() {
-  return <div>index</div>;
+  return (
+    <NavContainer>
+      <NavWrapper>
+        <NavList>
+          <Navitem>전체보기</Navitem>
+        </NavList>
+        <NavList>
+          <Navitem>양식</Navitem>
+        </NavList>
+        <NavList>
+          <Navitem>한식</Navitem>
+        </NavList>
+        <NavList>
+          <Navitem>중식</Navitem>
+        </NavList>
+        <NavList>
+          <Navitem>분식</Navitem>
+        </NavList>
+        <NavList>
+          <Navitem>야식</Navitem>
+        </NavList>
+        <NavList>
+          <Navitem>카페/디저트</Navitem>
+        </NavList>
+      </NavWrapper>
+    </NavContainer>
+  );
 }
 
 export default LocalNav;
