9abd64172c8c4dbda3977aff027c1e5b65815556
YHJ96 <yhj878812@naver.com>
2022-09-21 22:47:31 +0900

    feat: FoodTruck 컴포넌트 추가 완료

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -4,5 +4,6 @@ import { GlobalNav } from './GlobalNav';
 import { LocalNav } from './LocalNav';
 import { Spinner } from './Spinner';
 import { Thumbnail } from './Thumbnail';
+import { FoodTruck } from './FoodTruck';
 
-export { Banner, Form, GlobalNav, LocalNav, Spinner, Thumbnail };
+export { Banner, Form, GlobalNav, LocalNav, Spinner, Thumbnail, FoodTruck };
