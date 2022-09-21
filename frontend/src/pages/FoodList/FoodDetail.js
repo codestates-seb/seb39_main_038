8421270f66c6373ca49770b33e302e101363ae81
Jaewon Kim <99641988+Mark1237200@users.noreply.github.com>
2022-09-21 21:08:26 +0900

    Merge branch 'dev/fe' of github.com:Mark1237200/seb39_main_038_fork into dev/fe (#75)
    Co-authored-by: Mark1237200 <“0915kimjw@gmail.com”>

--- frontend/src/pages/FoodList/FoodDetail.js
+++ frontend/src/pages/FoodList/FoodDetail.js
@@ -9,13 +9,14 @@ import {
   CapsulizedInfo,
   Notice,
   StickyBody,
+  CartListBody,
   Cart,
   CartTab,
   CartList,
   CartListAdd,
   TotalPrice,
   OrderBtn,
-} from './Detailstyles';
+} from './FoodDetailstyles';
 
 function FoodDetail() {
   return (
@@ -49,33 +50,47 @@ function FoodDetail() {
             <div>장바구니</div>
             <button type="button">리셋</button>
           </CartTab>
+          <CartListBody>
+            <CartList>
+              <div>햄버거</div>
 
-          <CartList>
-            <div>햄버거</div>
+              <CartListAdd>
+                <div>8,000원</div>
+                <div className="수량버튼">
+                  <button type="button">-</button>
+                  <span>1</span>
+                  <button type="button">+</button>
+                </div>
+              </CartListAdd>
+            </CartList>
+            <CartList>
+              <div>피자</div>
 
-            <CartListAdd>
-              <div>8,000원</div>
-              <div className="수량버튼">
-                <button type="button">-</button>
-                <span>1</span>
-                <button type="button">+</button>
-              </div>
-            </CartListAdd>
-          </CartList>
-          <CartList>
-            <div>치킨</div>
+              <CartListAdd>
+                <div>18,000원</div>
+                <div className="수량버튼">
+                  <button type="button">-</button>
+                  <span>1</span>
+                  <button type="button">+</button>
+                </div>
+              </CartListAdd>
+            </CartList>
+            <CartList>
+              <div>치킨</div>
 
-            <CartListAdd>
-              <div>12,000원</div>
-              <div className="수량버튼">
-                <button type="button">-</button>
-                <span>2</span>
-                <button type="button">+</button>
-              </div>
-            </CartListAdd>
-          </CartList>
-          <TotalPrice className="totalPrice">합계: 20,000원</TotalPrice>
+              <CartListAdd>
+                <div>12,000원</div>
+                <div className="수량버튼">
+                  <button type="button">-</button>
+                  <span>1</span>
+                  <button type="button">+</button>
+                </div>
+              </CartListAdd>
+            </CartList>
+          </CartListBody>
+          <TotalPrice className="totalPrice">합계: 30,000원</TotalPrice>
         </Cart>
+
         <OrderBtn>
           <button type="button">주문 취소</button>
           <button type="button">바로 주문하기</button>
