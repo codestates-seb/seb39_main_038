3c99a1cd103fd10b22e811a5bff2024b34b06f8a
YHJ96 <yhj878812@naver.com>
2022-09-21 23:49:43 +0900

    feat: 경로 설정 완료

--- frontend/src/pages/FoodList/index.js
+++ frontend/src/pages/FoodList/index.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { Banner, LocalNav, FoodTruck } from '../../components';
-import { FoodListContainer, FoodListWrapper } from './styles';
+import { useLocation, Routes, Route } from 'react-router-dom';
+import { Banner, LocalNav } from '../../components';
+import FoodTrucks from './FoodTrucks';
+import FoodDetail from './FoodDetail';
 
 function FoodList() {
   const location = useLocation();
-  console.log(location.state.query);
+  console.log('FoodList', location.state?.query);
 
   return (
-    <div>
+    <>
       <Banner />
       <LocalNav />
-      <FoodListContainer>
-        <FoodListWrapper>
-          <FoodTruck />
-          <FoodTruck />
-          <FoodTruck />
-          <FoodTruck />
-        </FoodListWrapper>
-      </FoodListContainer>
-    </div>
+      <Routes>
+        <Route path="/" element={<FoodTrucks />} />
+        <Route path="/:id" element={<FoodDetail />} />
+      </Routes>
+    </>
   );
 }
 
-export default FoodList;
+export default React.memo(FoodList);
