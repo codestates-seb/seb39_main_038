98b08646975142610a1b85449fd40713e4c023a7
YHJ96 <yhj878812@naver.com>
2022-09-21 22:46:35 +0900

    design: FoodTruck 컴포넌트 와이어프레임 구현 완료

---
+++ frontend/src/components/FoodTruck/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  FoodTruckContainer,
+  FoodTruckWrapper,
+  FoodTruckImage,
+  FoodTruckTagBox,
+  FoodTruckTag,
+  FoodTruckTextInner,
+  FoodTruckTitle,
+  FoodTruckContentBox,
+  FoodTruckText,
+  FoodTruckTime,
+} from './styles';
+
+function FoodTruck({
+  title = '가게이름',
+  star = 4.5,
+  review = 100,
+  answer = 100,
+  tag = ['카드환영', '밤샘영업'],
+  time = '10분~20분',
+}) {
+  const createTag = () => {
+    let i = 0;
+    return tag.map((item) => {
+      i += 1;
+      return <FoodTruckTag key={i}>{item}</FoodTruckTag>;
+    });
+  };
+
+  return (
+    <FoodTruckContainer>
+      <FoodTruckWrapper>
+        <FoodTruckImage />
+        <FoodTruckTextInner>
+          <FoodTruckTitle>{title}</FoodTruckTitle>
+          <FoodTruckContentBox>
+            <FoodTruckText color="#ffa800">★ {star}</FoodTruckText>
+            <FoodTruckText>리뷰 {review}</FoodTruckText>
+            <FoodTruckText none>답변 {answer}</FoodTruckText>
+          </FoodTruckContentBox>
+          <FoodTruckTagBox>{createTag()}</FoodTruckTagBox>
+        </FoodTruckTextInner>
+        <FoodTruckTime>{time}</FoodTruckTime>
+      </FoodTruckWrapper>
+    </FoodTruckContainer>
+  );
+}
+
+export { FoodTruck };
