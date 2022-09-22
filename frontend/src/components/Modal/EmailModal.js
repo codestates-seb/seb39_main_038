84614c8cf1a426fc82b1b80ca932f96239eb1e05
YHJ96 <yhj878812@naver.com>
2022-09-23 00:11:21 +0900

    feat: EmailModal 컴포넌트 구현 완료

--- frontend/src/components/Modal/EmailModal.js
+++ frontend/src/components/Modal/EmailModal.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import { COLOR } from '../../constants';
+import { useRecoilValue } from 'recoil';
+import { atoms } from '../../store';
 import { Modal } from './Modal';
+import { EmailBody, EmailButton, EmailInput } from './styles';
 
-const EmailBody = styled.div`
-  display: flex;
-  height: calc(100% - 60px);
-  flex-direction: column;
-  justify-content: center;
-  gap: 20px;
-  padding: 10px;
-  font-size: 15px;
-`;
-
-const EmailInput = styled.input`
-  padding: 10px 15px;
-`;
-
-const EmailButton = styled.button`
-  padding: 10px 15px;
-  border: 0;
-  color: #fdfdfd;
-  background-color: ${COLOR.NAVY};
-`;
-
-function EmailModal() {
+function EmailModal({ closeModal }) {
+  const isMadal = useRecoilValue(atoms.modal);
+  if (!isMadal.email) return null;
   return (
-    <Modal title="이메일 확인" width={600} height={200}>
+    <Modal title="이메일 확인" width={600} height={200} closeModal={closeModal}>
       <EmailBody>
         <EmailInput placeholder="전송된 이메일의 인증번호를 입력하세요." />
         <EmailButton>인증번호 확인</EmailButton>
