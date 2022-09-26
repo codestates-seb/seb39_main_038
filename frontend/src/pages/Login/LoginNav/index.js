108189d8e32c0c25c4a2408e2196a54bf91fa0f2
YHJ96 <yhj878812@naver.com>
2022-09-26 21:15:00 +0900

    design: active 버튼 UI 추가 완료

--- frontend/src/pages/Login/LoginNav/index.js
+++ frontend/src/pages/Login/LoginNav/index.js
@@ -1,36 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
-import { COLOR } from '../../../constants';
+import { LoginNavContainer, ButtonWrapper, Button } from './styles';
 
-const LoginNavContainer = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  padding: 30px 0px;
-  justify-content: flex-end;
-  gap: 5px;
-`;
-
-const Button = styled.button`
-  font-size: 14px;
-  border: none;
-  font-weight: bold;
-  border: 1px solid ${COLOR.LIGHTNAVY};
-  color: ${COLOR.LIGHTNAVY};
-  background-color: transparent;
-  border-radius: 1px;
-  cursor: pointer;
-`;
-
-function LoginNav() {
+function LoginNav({ isChecked, setIsChecked }) {
+  const handleOnClick = (bool) => () => setIsChecked(bool);
   return (
     <LoginNavContainer>
       <ButtonWrapper>
-        <Button>일반 회원</Button>
-        <Button>사업자 회원</Button>
+        <Button active={!!isChecked} onClick={handleOnClick(true)}>
+          일반 회원
+        </Button>
+        <Button active={!isChecked} onClick={handleOnClick(false)}>
+          사업자 회원
+        </Button>
       </ButtonWrapper>
     </LoginNavContainer>
   );
