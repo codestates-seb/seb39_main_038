e5e33bd5db646e8da63301a4ceb4903ec2a3c4b0
YHJ96 <yhj878812@naver.com>
2022-09-27 00:21:38 +0900

    feat: 네비게이션 설정 추가 완료

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import LoginNav from './LoginNav';
 import ProviderLogin from './ProviderLogin';
 import ConsumerLogin from './ConsumerLogin';
 
 function Login() {
-  const [isChecked, setIsChecked] = useState(true);
+  const location = useLocation();
+  const toRegister = !location.state;
+  const [isChecked, setIsChecked] = useState(toRegister);
   return (
     <>
       <LoginNav isChecked={isChecked} setIsChecked={setIsChecked} />
