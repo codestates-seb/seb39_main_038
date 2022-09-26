984b6ba9bd6a0146265bf57c4cfda41064b196d4
YHJ96 <yhj878812@naver.com>
2022-09-26 14:54:30 +0900

    fix: REDIRECT_URI 수정 완료

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -3,7 +3,7 @@ const { API_HOST } = process.env;
 const KAKAO_API = {
   NAME: '카카오 로그인',
   CLIENT_ID: process.env.KAKAO_REST_API_KEY,
-  REDIRECT_URI: 'http://127.0.0.1:3000',
+  REDIRECT_URI: 'https://yapick.netlify.app',
   HOST: 'https://kauth.kakao.com',
   PATH() {
     return `/oauth/authorize?client_id=${this.CLIENT_ID}&redirect_uri=${this.REDIRECT_URI}&response_type=code`;
