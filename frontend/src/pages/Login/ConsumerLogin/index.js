0211c40352c3a23c220ad76c8e8c66d09a68390e
YHJ96 <yhj878812@naver.com>
2022-09-26 21:20:17 +0900

    feat: 카카오 로그인 기능 구현 완료

--- frontend/src/pages/Login/ConsumerLogin/index.js
+++ frontend/src/pages/Login/ConsumerLogin/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Form } from '../../../components';
+import { ButtonInner } from './styles';
+import { KAKAO_API } from '../../../constants';
 
 function ConsumerLogin() {
-  return <div>ConsumerLogin</div>;
+  const handleOnSubmit = (api) => (e) => {
+    e.preventDefault();
+    window.location.assign(api);
+  };
+
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    alert('준비중입니다...!!');
+  };
+
+  return (
+    <Form.Container onSubmit={handleOnSubmit(KAKAO_API.URI())}>
+      <Form.Wrapper>
+        <ButtonInner>
+          <Form.Button type="submit">카카오 로그인</Form.Button>
+          <Form.Button type="button" onClick={handleOnClick}>
+            구글 로그인
+          </Form.Button>
+          <Form.Button type="button" onClick={handleOnClick}>
+            페이스북 로그인
+          </Form.Button>
+          <Form.Button type="button" onClick={handleOnClick}>
+            깃허브 로그인
+          </Form.Button>
+        </ButtonInner>
+      </Form.Wrapper>
+    </Form.Container>
+  );
 }
 
 export default ConsumerLogin;
