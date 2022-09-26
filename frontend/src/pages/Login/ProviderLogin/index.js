4f2772db96304a38585f5c1481926027e1000103
YHJ96 <yhj878812@naver.com>
2022-09-26 21:50:50 +0900

    feat: 로그인 유효성 검사 기능 구현 완료

--- frontend/src/pages/Login/ProviderLogin/index.js
+++ frontend/src/pages/Login/ProviderLogin/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Form } from '../../../components';
 import { Logo, LoginInner, FindInner, FindText } from './styles';
-import { ROUTE } from '../../../constants';
+import { ROUTE, COLOR } from '../../../constants';
+// import { sha256 } from '../../../utils';
+
+/*
+  비밀번호 규칙
+  1. 10자리 이상
+  2. 숫자 영문 특수문자
+*/
 
 function ProviderLogin() {
   const navigate = useNavigate();
+
+  const validation = (email, pw) => {
+    if (!(email && pw)) return 401;
+    if (pw.length < 10) return 402;
+    return null;
+  };
+
   const hanldeOnSumbmit = (e) => {
     e.preventDefault();
-    e.reset();
+    const { id, pw } = e.target;
+    const isCheck = validation(id.value, pw.value);
+    if (isCheck === 401) return alert('이메일과 비밀번호 모두 입력해주세요.');
+    if (isCheck === 402) return alert('비밀번호는 10자 이상입니다.');
+    return null;
   };
 
   const hanldeOnClickText = (path) => () => navigate(`/${path}`);
@@ -32,8 +50,18 @@ function ProviderLogin() {
         </FindInner>
 
         <LoginInner>
-          <Form.Button type="submit">{ROUTE.LOGIN.NAME}</Form.Button>
-          <Form.Button onClick={hanldeOnClickText(ROUTE.REGISTER.PATH)}>
+          <Form.Button
+            type="submit"
+            color={COLOR.YELLOW}
+            fontColor={COLOR.WHITE}
+          >
+            {ROUTE.LOGIN.NAME}
+          </Form.Button>
+          <Form.Button
+            color={COLOR.NAVY}
+            fontColor={COLOR.WHITE}
+            onClick={hanldeOnClickText(ROUTE.REGISTER.PATH)}
+          >
             {ROUTE.REGISTER.NAME}
           </Form.Button>
         </LoginInner>
