4ca990810ba19751960e64185e43da3c191d6931
YHJ96 <yhj878812@naver.com>
2022-09-26 21:15:44 +0900

    feat: 탭 네비게이션 기능 추가 완료

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginNav from './LoginNav';
 import ProviderLogin from './ProviderLogin';
+import ConsumerLogin from './ConsumerLogin';
 
 function Login() {
+  const [isChecked, setIsChecked] = useState(true);
   return (
     <>
-      <LoginNav />
-      <ProviderLogin />
+      <LoginNav isChecked={isChecked} setIsChecked={setIsChecked} />
+      {isChecked ? <ConsumerLogin /> : <ProviderLogin />}
     </>
   );
 }
