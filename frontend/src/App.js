131aba48423cdbb263e606fcbb4217a930a2d7ca
YHJ96 <yhj878812@naver.com>
2022-09-26 11:32:26 +0900

    feat: MyPage & NotFound 페이지 경로 추가 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ const PwInquiry = React.lazy(() => import('./pages/PwInquiry'));
 const FoodList = React.lazy(() => import('./pages/FoodList'));
 const Basket = React.lazy(() => import('./pages/Basket'));
 const Order = React.lazy(() => import('./pages/Order'));
+const MyPage = React.lazy(() => import('./pages/MyPage'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
@@ -25,6 +27,8 @@ function App() {
         <Route path={ROUTE.PWINQUIRY.PATH} element={<PwInquiry />} />
         <Route path={ROUTE.BASKET.PATH} element={<Basket />} />
         <Route path={ROUTE.ORDER.PATH} element={<Order />} />
+        <Route path={ROUTE.MYPAGE.PATH} element={<MyPage />} />
+        <Route path={ROUTE.NOTFOUND.PATH} element={<NotFound />} />
       </Routes>
     </React.Suspense>
   );
