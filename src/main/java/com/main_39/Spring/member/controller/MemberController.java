60580eaf329fa3ad53fb297a62e7236fd02dc025
ds02168 <1995musso@gmail.com>
2022-09-26 15:25:20 +0900

    feat: 쿠키에 domain 옵션 추가

--- src/main/java/com/main_39/Spring/member/controller/MemberController.java
+++ src/main/java/com/main_39/Spring/member/controller/MemberController.java
@@ -107,6 +107,7 @@ public class MemberController {
         //access_token 삭제
         ResponseCookie remove_access_cookie = ResponseCookie.from("kakao_access_token",access_token)
                 .path("/")
+                .domain("https://yapick.netlify.app")
                 .sameSite("None")
                 .secure(true)
                 .httpOnly(true)
@@ -118,6 +119,7 @@ public class MemberController {
         //refresh_token 삭제
         ResponseCookie remove_refresh_cookie = ResponseCookie.from("kakao_refresh_token",refresh_token)
                 .path("/")
+                .domain("https://yapick.netlify.app")
                 .sameSite("None")
                 .secure(true)
                 .httpOnly(true)
@@ -152,6 +154,7 @@ public class MemberController {
         //access_token 쿠키
         ResponseCookie access_cookie = ResponseCookie.from("kakao_access_token", oauthToken.getAccess_token())
                 .path("/")
+                .domain("https://yapick.netlify.app")
                 .sameSite("None")
                 .secure(true)
                 .httpOnly(true)
@@ -162,6 +165,7 @@ public class MemberController {
         //refresh_token 쿠키
         ResponseCookie refresh_cookie = ResponseCookie.from("kakao_refresh_token",oauthToken.getRefresh_token())
                 .path("/")
+                .domain("https://yapick.netlify.app")
                 .sameSite("None")
                 .secure(true)
                 .httpOnly(true)
