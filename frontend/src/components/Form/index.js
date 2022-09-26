fe675051d49f047007bcc700ebfc3c3f3fcd6f70
YHJ96 <yhj878812@naver.com>
2022-09-26 19:07:33 +0900

    design: height 수정 완료

--- frontend/src/components/Form/index.js
+++ frontend/src/components/Form/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
   max-width: 800px;
   margin: 0 auto;
-  height: calc(100vh - 80px);
+  height: calc(100vh - 164px);
   display: flex;
   flex-direction: column;
   justify-content: center;
