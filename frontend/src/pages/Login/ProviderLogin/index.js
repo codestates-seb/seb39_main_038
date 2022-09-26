33b738bd47997025fc465d32b3dc198e545351f9
YHJ96 <yhj878812@naver.com>
2022-09-26 19:15:19 +0900

    refactor: ProviderLogin 컴포넌트 분리 완료

---
+++ frontend/src/pages/Login/ProviderLogin/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form } from '../../../components';
+import { Logo, LoginInner, FindInner, FindText } from './styles';
+import { KAKAO_API, ROUTE } from '../../../constants';
+
+function ProviderLogin() {
+  const navigate = useNavigate();
+  const hanldeOnSumbmit = (e) => {
+    e.preventDefault();
+    // const { id, pw } = e.target;
+    e.reset();
+  };
+  const handleOnClick = (api) => () => window.location.assign(api);
+  const hanldeOnClickText = (path) => () => navigate(`/${path}`);
+
+  return (
+    <Form.Container>
+      <Logo />
+      <Form.Wrapper onSubmit={hanldeOnSumbmit}>
+        <LoginInner>
+          <Form.Input name="id" type="email" placeholder="이메일 주소 입력" />
+          <Form.Input name="pw" type="password" placeholder="비밀번호 입력" />
+        </LoginInner>
+
+        <FindInner>
+          <FindText onClick={hanldeOnClickText(ROUTE.IDINQUIRY.PATH)}>
+            {ROUTE.IDINQUIRY.NAME}
+          </FindText>
+          <FindText onClick={hanldeOnClickText(ROUTE.PWINQUIRY.PATH)}>
+            {ROUTE.PWINQUIRY.NAME}
+          </FindText>
+        </FindInner>
+
+        <LoginInner>
+          <Form.Button type="submit">{ROUTE.LOGIN.NAME}</Form.Button>
+          <Form.Button onClick={handleOnClick(KAKAO_API.URI())}>
+            {KAKAO_API.NAME}
+          </Form.Button>
+          <Form.Button onClick={hanldeOnClickText(ROUTE.REGISTER.PATH)}>
+            {ROUTE.REGISTER.NAME}
+          </Form.Button>
+        </LoginInner>
+      </Form.Wrapper>
+    </Form.Container>
+  );
+}
+
+export default ProviderLogin;
