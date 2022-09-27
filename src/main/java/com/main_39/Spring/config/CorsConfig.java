830181b864545973d384e310c044527f7bf935dd
ds02168 <84536269+ds02168@users.noreply.github.com>
2022-09-27 16:23:40 +0900

    fix: Access-Control-Allow-Header 속성 수정 (#124)
    * feat: addExposedHeader 속성 추가
    
    * fix: domain 주소 수정
    
    * fix: domain 주소 수정
    
    * fix: domain 주소 수정
    
    * feat: java doc 추가, Store 엔티티, DTO추가
    
    * rename: 프론트엔드와의 JSON 필드명 맞춤
    
    * fix : localhost:3000 Origin 추가
    
    * fix : https://deploymentprac38.tk alllow-origin 추가
    
    * feat : Menu : Store N:1 관계 추가
    
    * fix: 비밀번호 검사 x
    
    * fix: domain 속성 제거
    
    * fix: Access-Control-Allow-Header 속성 수정

--- src/main/java/com/main_39/Spring/config/CorsConfig.java
+++ src/main/java/com/main_39/Spring/config/CorsConfig.java
@@ -19,9 +19,8 @@ public class CorsConfig {
         config.addAllowedOrigin("https://yapick.netlify.app");
         config.addAllowedOrigin("https://deploymentprac38.tk");
         config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedHeader("*");
+        config.addAllowedHeader("Origin, X-Requested-With, Content-Type, Accept, Authorization");
         config.addAllowedMethod("*");
-        config.addExposedHeader("*");
         source.registerCorsConfiguration("/**",config);
 
         return new CorsFilter(source);
