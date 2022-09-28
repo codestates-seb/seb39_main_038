dce11f72fc040342f7317f55eeecd8f8c6f10a3f
YHJ96 <yhj878812@naver.com>
2022-09-29 01:01:05 +0900

    feat: GlobalNav 로그인 상태 구현 완료

--- frontend/src/components/GlobalNav/index.js
+++ frontend/src/components/GlobalNav/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 import {
   NavContainer,
   NavWrapper,
@@ -8,24 +9,47 @@ import {
   NavButton,
 } from './styles';
 import { ROUTE, COLOR } from '../../constants';
+import { atoms } from '../../store';
 
 function GlobalNav() {
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useRecoilState(atoms.isLogin);
+
+  const handleOnClick = () => {
+    const isCheck = window.confirm('정말 로그아웃 하시겠습니까?');
+    if (!isCheck) return;
+    setIsLogin(false);
+    navigate(ROUTE.HOME, { replace: true });
+  };
+
+  const isLoginComponent = () => {
+    if (isLogin)
+      return (
+        <ButtonInner>
+          <NavButton onClick={handleOnClick} color={COLOR.YELLOW}>
+            {ROUTE.LOGOUT.NAME}
+          </NavButton>
+          <Link to={ROUTE.BASKET.PATH}>
+            <NavButton color={COLOR.WHITE}>{ROUTE.BASKET.NAME}</NavButton>
+          </Link>
+        </ButtonInner>
+      );
+
+    return (
+      <ButtonInner>
+        <Link to={ROUTE.LOGIN.PATH}>
+          <NavButton color={COLOR.YELLOW}>{ROUTE.LOGIN.NAME}</NavButton>
+        </Link>
+      </ButtonInner>
+    );
+  };
   return (
     <NavContainer>
       <NavWrapper>
         <Link to={ROUTE.HOME.PATH}>
           <NavLogo />
         </Link>
-        <ButtonInner>
-          <Link to={ROUTE.LOGIN.PATH}>
-            <NavButton color={COLOR.YELLOW}>{ROUTE.LOGIN.NAME}</NavButton>
-          </Link>
-          {true ? null : (
-            <Link to={ROUTE.BASKET.PATH}>
-              <NavButton color={COLOR.WHITE}>{ROUTE.BASKET.NAME}</NavButton>
-            </Link>
-          )}
-        </ButtonInner>
+        {isLoginComponent()}
       </NavWrapper>
     </NavContainer>
   );
