fbf7fa75d9c921dd8f14cb9240781d00a588687e
YHJ96 <yhj878812@naver.com>
2022-09-29 01:00:29 +0900

    feat: 로그인 예외 처리 기능 구현 완료

--- frontend/src/pages/Login/ProviderLogin/index.js
+++ frontend/src/pages/Login/ProviderLogin/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useSetRecoilState } from 'recoil';
+import { atoms } from '../../../store';
 import { Form } from '../../../components';
 import { Logo, LoginInner, FindInner, FindText } from './styles';
 import { ROUTE, COLOR, ALERT, API_URI } from '../../../constants';
 import { sha256 } from '../../../utils';
 
 function ProviderLogin() {
+  const setIsLogin = useSetRecoilState(atoms.isLogin);
   const navigate = useNavigate();
 
   const postUserData = async (email, password) => {
@@ -14,10 +17,10 @@ function ProviderLogin() {
       email,
       password,
     };
-    console.log('userInfo', userInfo);
     const response = await axios.post(API_URI.LOGIN, userInfo);
-    console.log('response', response);
-    return navigate(`/${ROUTE.LOGIN.PATH}`, { state: ROUTE.REGISTER.PATH });
+    if (response.status === 226) return alert(response.data?.message);
+    setIsLogin(true);
+    return navigate(ROUTE.HOME.PATH);
   };
 
   const validation = (email, pw) => {
@@ -31,9 +34,7 @@ function ProviderLogin() {
     const { email, password } = e.target;
     const isCheck = validation(email.value, password.value);
     if (ALERT.CLIENT[isCheck]) return alert(ALERT.CLIENT[isCheck].MESSAGE);
-    postUserData(email.value, sha256(password.value)).catch(() =>
-      alert(ALERT.CLIENT[500].MESSAGE),
-    );
+    postUserData(email.value, sha256(password.value));
     return null;
   };
 
