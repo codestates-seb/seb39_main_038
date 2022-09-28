359c213ea1b86b1cc2d376e2a6cfde85c2420c70
YHJ96 <yhj878812@naver.com>
2022-09-28 23:09:46 +0900

    design: MyPage 컴포넌트 와이어프레임 구현 완료

--- frontend/src/pages/MyPage/index.js
+++ frontend/src/pages/MyPage/index.js
@@ -1,81 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const MyPageContainer = styled.div`
-  max-width: 1020px;
-  margin: 0 auto;
-`;
-
-const InfoInner = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 1px solid #000;
-`;
-
-const AvatarBox = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const TextBox = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Text = styled.span``;
-
-const Avatar = styled.img`
-  border: 1px solid #000;
-  width: 64px;
-  height: 64px;
-`;
-
-const Button = styled.button`
-  width: 100%;
-`;
-
-const OrderInner = styled.div`
-  border: 1px solid #000;
-`;
-
-const OrderHeader = styled.h1``;
-
-const OrderContent = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const ButtonBox = styled.div`
-  display: flex;
-  align-self: flex-end;
-`;
+import {
+  MyPageContainer,
+  InfoInner,
+  Header,
+  InfoContent,
+  TextBox,
+  Text,
+  AvatarBox,
+  Avatar,
+  OrderInner,
+  OrderContent,
+  ButtonBox,
+  Button,
+} from './styles';
 
 function MyPage() {
   return (
     <MyPageContainer>
       <InfoInner>
-        <TextBox>
-          <Text>
-            닉네임: <Text>양희준</Text>
-          </Text>
-          <Text>
-            이메일:
-            <Text>codestate@gmail.com</Text>
-          </Text>
-          <Text>마일리지 0 원</Text>
-        </TextBox>
-        <AvatarBox>
-          <Avatar alt="avatar" />
-          <Button>수정</Button>
-        </AvatarBox>
+        <Header>유저정보</Header>
+        <InfoContent>
+          <TextBox>
+            <Text size={14}>닉네임: 양희준</Text>
+            <Text size={14}>이메일: codestate@gmail.com</Text>
+            <Text size={14}>마일리지: 0 원</Text>
+          </TextBox>
+          <AvatarBox>
+            <Avatar alt="avatar" />
+            <Button>수정</Button>
+          </AvatarBox>
+        </InfoContent>
       </InfoInner>
       <OrderInner>
-        <OrderHeader>주문조회</OrderHeader>
+        <Header>주문조회</Header>
         <OrderContent>
           <TextBox>
-            <Text>KFC</Text>
-            <Text>주문날짜: 2020.09.21</Text>
+            <Text size={14}>KFC</Text>
+            <Text size={12} color="#999999">
+              주문날짜: 2020.09.21
+            </Text>
           </TextBox>
           <ButtonBox>
             <Button>리뷰쓰기</Button>
