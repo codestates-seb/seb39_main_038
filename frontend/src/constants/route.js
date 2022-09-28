b76beacb0b6ddd13f78e4298ea8f1524f578d0a7
YHJ96 <yhj878812@naver.com>
2022-09-29 00:59:44 +0900

    feat: logout 경로 추가 완료

--- frontend/src/constants/route.js
+++ frontend/src/constants/route.js
@@ -10,6 +10,7 @@ const ROUTE = {
     FOODTRUCKSETTING: { PATH: '/' },
   },
   LOGIN: { PATH: 'login', NAME: '로그인' },
+  LOGOUT: { PATH: 'logout', NAME: '로그아웃' },
   REGISTER: { PATH: 'register', NAME: '회원가입' },
   IDINQUIRY: { PATH: 'idinquiry', NAME: '아이디 찾기' },
   PWINQUIRY: { PATH: 'pwinquiry', NAME: '비밀번호 찾기' },
