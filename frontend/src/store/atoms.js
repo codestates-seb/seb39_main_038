d04b19d395a93dee48470374ea7306dd91704723
YHJ96 <yhj878812@naver.com>
2022-09-29 15:16:52 +0900

    feat: isLogin atom-Effect 추가 완료

--- frontend/src/store/atoms.js
+++ frontend/src/store/atoms.js
@@ -4,8 +4,23 @@ import { atom, selector } from 'recoil';
 import { storge } from './storge';
 import { MENU } from '../constants';
 
+const sessionStorageEffect =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const data = storge.getData(key);
+    if (data) setSelf(data);
+    onSet((newData) => {
+      storge.setData(key, newData);
+    });
+  };
+
 const atoms = {
-  isLogin: atom({ key: 'isLogin', default: storge.getData('isLogin') }),
+  isLogin: atom({
+    key: 'isLogin',
+    default: { state: false, type: null },
+    effects: [sessionStorageEffect('isLogin')],
+  }),
+
   menuQuery: atom({ key: 'menu', default: MENU[0].query }),
   menuTab: atom({ key: 'menuTab', default: '메뉴' }),
   modal: atom({
