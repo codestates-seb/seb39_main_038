6502e29a6c9671bd1ca48070407f14926fd1cccb
YHJ96 <yhj878812@naver.com>
2022-10-01 00:27:09 +0900

    design: Review 컴포넌트 UI & CSS 구현 완료

---
+++ frontend/src/pages/Review/styles.js
@@ -0,0 +1,83 @@
+import styled, { css } from 'styled-components';
+
+const Container = styled.div`
+  max-width: 660px;
+  margin: 0 auto;
+  padding: 10px 0px;
+`;
+
+const EditorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 15px;
+  gap: 10px;
+`;
+
+const Button = styled.button``;
+
+const FileInput = styled.input`
+  display: none;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Editor = styled.textarea`
+  resize: none;
+  height: 180px;
+  border: 1px solid #ccc;
+  font-size: 14px;
+  color: #666666;
+`;
+
+const ViewWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 15px;
+  gap: 10px;
+`;
+
+const View = styled.div`
+  border: 1px solid #ccc;
+  padding: 10px 15px;
+  ${({ isUrl }) =>
+    isUrl &&
+    css`
+      height: 180px;
+    `}
+`;
+
+const ViewImage = styled.div`
+  width: 100%;
+  height: 350px;
+  background: url(${({ url }) => url});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  ${({ url }) =>
+    url === null &&
+    css`
+      display: none;
+    `}
+`;
+
+const Text = styled.span`
+  font-size: ${({ size }) => size};
+  color: ${({ color }) => color};
+`;
+
+export {
+  Container,
+  EditorWrapper,
+  Button,
+  FileInput,
+  Header,
+  Editor,
+  ViewWrapper,
+  View,
+  ViewImage,
+  Text,
+};
