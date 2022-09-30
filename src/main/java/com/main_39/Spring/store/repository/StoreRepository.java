f2ad64e814134fe674954470ac174a96675c27dc
kal1777 <kimnayul17@gmail.com>
2022-09-30 16:53:34 +0900

    feat: aws image 업로드 복원

--- src/main/java/com/main_39/Spring/store/repository/StoreRepository.java
+++ src/main/java/com/main_39/Spring/store/repository/StoreRepository.java
@@ -4,7 +4,6 @@ import com.main_39.Spring.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-
 import java.util.Optional;
 
 
