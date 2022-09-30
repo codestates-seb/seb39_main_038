d7de096045a1454fc04320381b1f81be103a3377
kal1777 <kimnayul17@gmail.com>
2022-09-30 13:39:49 +0900

    feat: s3 이미지 업로드

--- src/main/java/com/main_39/Spring/store/repository/StoreRepository.java
+++ src/main/java/com/main_39/Spring/store/repository/StoreRepository.java
@@ -1,9 +1,12 @@
 package com.main_39.Spring.store.repository;
 
 import com.main_39.Spring.store.entity.Store;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 
 
