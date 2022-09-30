e13b6f2108c88249e3b6609687012ea732d6600e
YHJ96 <yhj878812@naver.com>
2022-09-30 18:27:38 +0900

    fix: 프로퍼티 카멜케이스로 변경완료

--- server/data/foodList.js
+++ server/data/foodList.js
@@ -22,15 +22,15 @@ const tag = ["카드 환영", "시원한 맥주", "얼음물 무료", "맛집"];
 
 for (let i = 0; i < data.length; i++) {
   const result = {
-    store_id: i + 1,
-    local_id: i + 1,
-    store_status: true,
-    store_name: name[randomRange(0, 4)],
-    store_image:
+    storeId: i + 1,
+    localId: i + 1,
+    storeStatus: true,
+    storeName: name[randomRange(0, 4)],
+    storeImage:
       "https://www.yogiyo.co.kr/mobile/image/default_restaurant_logo.png",
-    store_type: null,
-    store_waittime: waittime[randomRange(0, 5)],
-    store_tag: tag[randomRange(0, 4)],
+    storeType: null,
+    storeWaittime: waittime[randomRange(0, 5)],
+    storeTag: tag[randomRange(0, 4)],
   };
   data[i] = result;
 }
