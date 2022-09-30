af0b965e8c416e4d740e15940af1ede7b1277a93
Jaewon Kim <99641988+Mark1237200@users.noreply.github.com>
2022-09-30 18:32:18 +0900

    feat: CRUD 가게페이지 완료, Detail 구성중 & 가게페이지 드랍다운 완성 (#153)
    * Design: FoodDetail CSS 추가 작업 완료
    
    * Refectory: CSS 마진, Width, Height 제거 후 리펙토링 중
    
    * Refectory: CSS 마진, Width, Height 제거 후 리펙토링 중
    
    * Refectory: FoodDetail CSS 리팩토링 완료
    
    * Design: 가게설정 페이지 CSS 작업 완료
    
    * Fix: FoodDetail 메뉴바 오류 수정 완료
    
    * feat: 통신코드 작업중 & 태그 인풋 CSS 작업중
    
    * fix: 수인님과 CRUD 구성
    
    * feat: ThemeProvider 제거, 디테일 메뉴탭 분기처리 리액트 쿼리 완료
    
    * Design: 가게설정 페이지 드랍다운 완성
    
    * feat: CRUD 가게페이지 완료, Detail 구성중 & 가게페이지 드랍다운 완성
    
    * fix: 반응형 CSS 버그 수정 & 리액트 쿼리 무한 get 재통신 버그 수정완
    
    Co-authored-by: Mark1237200 <“0915kimjw@gmail.com”>

--- frontend/src/components/DetailReview/index.js
+++ frontend/src/components/DetailReview/index.js
@@ -1,4 +1,7 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
+import { Spinner } from '../Spinner';
 import {
   Section,
   Rating,
@@ -13,32 +16,72 @@ import {
   Thumnail,
 } from './styles';
 
+function ReviewList() {
+  const queryClient = useQueryClient();
+
+  const getReviewList = async () => {
+    const res = await axios.get('/review');
+    return res;
+  };
+
+  const { isLoading, isError, data, error } = useQuery(
+    ['getReview'],
+    getReviewList,
+    {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: 1,
+      retryDelay: 3000,
+
+      onSuccess: () => {
+        alert('리뷰 불러오기 성공');
+        queryClient.invalidateQueries(['getReview']);
+      },
+
+      onError: (e) => {
+        alert(e);
+      },
+    },
+  );
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return alert('리뷰 불러오기 실패', error);
+  }
+
+  return data.map((res) => (
+    <Comment key={res.id}>
+      <NameDateReply>
+        <div>
+          {res.name} <Date>{res.date}</Date>
+        </div>
+        <div>
+          <ReplyDeleteBtn type="button">답글</ReplyDeleteBtn>
+          <ReplyDeleteBtn type="button">삭제</ReplyDeleteBtn>
+        </div>
+      </NameDateReply>
+      <Rate>별점 {res.rate}</Rate>
+      <ThumnailBox>
+        <Thumnail alt="Thumnail" src={res.img} />
+      </ThumnailBox>
+      <OrderHistory>{res.history}</OrderHistory>
+      <div>{res.content}</div>
+    </Comment>
+  ));
+}
+
 function DetailReview() {
   return (
     <Section>
       <Rating>
         <TotalRate>5.0</TotalRate> <span>★★★★★</span>
       </Rating>
-      <Comment>
-        <NameDateReply>
-          <div>
-            김재원 <Date>2022-09-20</Date>
-          </div>
-          <div>
-            <ReplyDeleteBtn type="button">답글</ReplyDeleteBtn>
-            <ReplyDeleteBtn type="button">삭제</ReplyDeleteBtn>
-          </div>
-        </NameDateReply>
-        <Rate>별점 ★★</Rate>
-        <ThumnailBox>
-          <Thumnail alt="Thumnail" />
-        </ThumnailBox>
-        <OrderHistory>주문내역</OrderHistory>
-        <div>
-          사장님도 친절하시고, 양도 많고 엄청 신선했습니다! 많이 시켜먹을 것
-          같습니다!
-        </div>
-      </Comment>
+
+      {ReviewList}
+
       <Comment>
         <NameDateReply>
           <div>
--- frontend/src/store/atoms.js
+++ frontend/src/store/atoms.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
-
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 import { storge } from './storge';
 import { MENU } from '../constants';
 
@@ -22,32 +20,10 @@ const atoms = {
   }),
 
   menuQuery: atom({ key: 'menu', default: MENU[0].query }),
-  menuTab: atom({ key: 'menuTab', default: '메뉴' }),
   modal: atom({
     key: 'modal',
     default: { food: false, order: false, email: false },
   }),
-  tagName: atom({ key: 'tagName', default: '' }),
 };
 
-const selectors = {
-  getCart: selector({
-    key: 'getCart',
-    get: async () => {
-      const res = await axios.get('http://localhost:8080/order/1');
-      return res.data;
-    },
-  }),
-  getMenu: selector({
-    key: 'getMenu',
-    get: async () => {
-      const res = await axios.get('/store/1');
-      if (res.menu) {
-        return res.menu;
-      }
-      return alert('음식을 불러오지 못했습니다.');
-    },
-  }),
-};
-
-export { atoms, selectors };
+export { atoms };
