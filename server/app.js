845ec9a2cba412219e561ee1c976559c991f4b73
YHJ96 <yhj878812@naver.com>
2022-10-03 20:08:57 +0900

    feat: 푸드 리스트 데이터 & API 기능 구현 완료

--- server/app.js
+++ server/app.js
@@ -10,7 +10,26 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.get("/store", (req, res) => {
-  return res.status(200).json(foodListData);
+  const { page, size, type } = req.query;
+
+  const pageInfo = (data) => ({
+    page,
+    size,
+    totalElements: data.length,
+    totalPages: parseInt(data.length / size, 10) + 1,
+  });
+
+  if (type === "all")
+    return res.status(200).json({
+      data: foodListData.slice((page - 1) * size, page * size),
+      pageInfo: pageInfo(foodListData),
+    });
+
+  const filterData = foodListData.filter((item) => type === item.storeType);
+  return res.status(200).json({
+    data: filterData.slice((page - 1) * size, page * size),
+    pageInfo: pageInfo(filterData),
+  });
 });
 
 app.listen(PORT, () => {
