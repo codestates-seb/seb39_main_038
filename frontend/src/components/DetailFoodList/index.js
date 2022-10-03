187e8336667cdc017522aedfa159fed12caa1789
YHJ96 <yhj878812@naver.com>
2022-10-04 02:56:53 +0900

    fix: 리코일 형식 수정중

--- frontend/src/components/DetailFoodList/index.js
+++ frontend/src/components/DetailFoodList/index.js
@@ -1,79 +1,43 @@
-import axios from 'axios';
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
 import React from 'react';
-import { Spinner } from '../Spinner';
-import { Section, Menu, MenuInfo, Name, Info, Price, MenuImg } from './styles';
+import { useSetRecoilState } from 'recoil';
 import { atoms } from '../../store';
+import { CustomModal } from '../CustomModal';
+import { Section, Menu, MenuInfo, Name, Info, Price, MenuImg } from './styles';
+import { useModal } from '../../hooks';
 
-function FoodMenuList() {
-  const [receipt, setReceipt] = useRecoilState(atoms.menuOrder);
-  const setOrderList = useSetRecoilState(atoms.orderList);
-
-  const [defaultObj, ...rest] = receipt;
-  setOrderList(rest);
-  console.log(defaultObj);
-  const queryClient = useQueryClient();
-  queryClient.invalidateQueries(['getMenu']);
-
-  const getMenuList = async () => {
-    const res = await axios.get('http://localhost:8080/store');
-    return res.data.menus;
-  };
-
-  const { isLoading, isError, data, error } = useQuery(
-    ['getMenu'],
-    getMenuList,
-    {
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      retry: 1,
-      retryDelay: 3000,
-
-      onSuccess: () => {
-        alert('성공');
-      },
-
-      onError: (e) => {
-        alert(e.message);
-      },
-    },
-  );
-
-  if (isLoading) {
-    return <Spinner />;
-  }
-
-  if (isError) {
-    return alert('음식을 불러오지 못했습니다.', error.message);
-  }
-  console.log(receipt);
-  return data.map((menu) => (
-    <Menu
-      key={menu.id}
-      onClick={() => {
-        return setReceipt([
-          ...receipt,
-          { name: menu.name, price: menu.price, id: menu.id },
-        ]);
-      }}
-    >
-      <MenuInfo>
-        <Name>{menu.name}</Name>
-        <Info>{menu.info}</Info>
-        <Price>{menu.price}</Price>
-      </MenuInfo>
-      <MenuImg>
-        <img alt="menuImg" src={menu.img} />
-      </MenuImg>
-    </Menu>
-  ));
-}
+const foods = [
+  { name: '치킨', info: '바삭한 치킨', price: 10000, img: null },
+  { name: '피자', info: '바삭한 피자', price: 10000, img: null },
+];
 
 function DetailFoodList() {
+  const [openFood, closeFood] = useModal('food');
+  const setMenuOrder = useSetRecoilState(atoms.menuOrder);
+
+  const handleOnClick = (data) => () => {
+    openFood();
+    setMenuOrder({ ...data });
+  };
+
+  const createFoodMenuList = () => {
+    return foods.map((menu) => (
+      <Menu key={menu.name} onClick={handleOnClick(menu)}>
+        <MenuInfo>
+          <Name>{menu.name}</Name>
+          <Info>{menu.info}</Info>
+          <Price>{menu.price}</Price>
+        </MenuInfo>
+        <MenuImg>
+          <img alt="menuImg" src={menu.img} />
+        </MenuImg>
+      </Menu>
+    ));
+  };
+
   return (
     <Section>
-      <FoodMenuList />
+      {createFoodMenuList()}
+      <CustomModal.Food closeModal={closeFood} />
     </Section>
   );
 }
