51a788d22091f5e9f07a3fc283c7e018981a6686
YHJ96 <yhj878812@naver.com>
2022-10-03 22:09:57 +0900

    feat: 무한 스크롤 구현중

--- frontend/src/pages/FoodList/FoodTrucks/index.js
+++ frontend/src/pages/FoodList/FoodTrucks/index.js
@@ -4,16 +4,23 @@ import { FoodTruck } from '../../../components';
 import { useFoodList } from '../../../hooks';
 
 function FoodTrucks() {
-  const { data } = useFoodList();
+  const { data, fetchNextPage } = useFoodList();
 
   const createFoodTruck = () => {
-    return data?.data.data.map((item) => {
+    const result = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const page of data.pages) result.push(...page.data.data);
+
+    return result.map((item) => {
       return <FoodTruck key={item.storeId} id={item} data={item} />;
     });
   };
 
   return (
     <FoodListContainer>
+      <button type="button" onClick={fetchNextPage}>
+        클릭
+      </button>
       <FoodListWrapper>{createFoodTruck()}</FoodListWrapper>
     </FoodListContainer>
   );
