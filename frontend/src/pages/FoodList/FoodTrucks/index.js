856c7ed43527e740dbda2da9431db1a11a6d0c02
YHJ96 <yhj878812@naver.com>
2022-10-03 18:30:41 +0900

    refactor: 주석 제거 완료

--- frontend/src/pages/FoodList/FoodTrucks/index.js
+++ frontend/src/pages/FoodList/FoodTrucks/index.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
 import { FoodListContainer, FoodListWrapper } from './styles';
 import { FoodTruck } from '../../../components';
-import { API_URI } from '../../../constants';
-
-/*
-  /store?page={page}&size={size}&type={all}
-
-  data: [
-    store_id" : Long,
-    "local_id" : Long,
-    "store_status" : String,
-    "store_name" : String,
-    "store_image" : String,
-    "store_type" : String,
-    "store_waittime" : String,
-  ]
-*/
-
-const fetchFoodList = async () => {
-  const response = await axios.get(API_URI.FOODLIST);
-  return response;
-};
+import { useFoodList } from '../../../hooks';
 
 function FoodTrucks() {
-  const { data, isError, error } = useQuery(['foodlist'], fetchFoodList);
-
-  if (isError) return <div>{error.message}</div>;
+  const { data } = useFoodList();
 
   const createFoodTruck = () => {
     return data?.data.map((item) => {
