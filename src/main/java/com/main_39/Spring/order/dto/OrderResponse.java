4a1a01b417f79f59d36f5f672c81b309da89e15d
lkc9898 <104333737+lkc9898@users.noreply.github.com>
2022-10-04 18:49:35 +0900

    refactor: 주문요청사항 추가, 주문 리스트, 상세 주문 응답 수정

--- src/main/java/com/main_39/Spring/order/dto/OrderResponse.java
+++ src/main/java/com/main_39/Spring/order/dto/OrderResponse.java
@@ -5,24 +5,32 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 public class OrderResponse {
     private long orderId;
 
+    private List<OrderMenuResponse> orderMenu = new ArrayList<>();
+
     private int totalCount;
 
     private int totalPrice;
+
+    private String orderRequest;
 
     private PaymentType paymentType;
 
     private LocalDateTime createdAt;
 
     @Builder
-    public OrderResponse(long orderId, int totalCount, int totalPrice, PaymentType paymentType, LocalDateTime createdAt) {
+    public OrderResponse(long orderId, List<OrderMenuResponse> orderMenu, int totalCount, int totalPrice, String orderRequest, PaymentType paymentType, LocalDateTime createdAt) {
         this.orderId = orderId;
+        this.orderMenu = orderMenu;
         this.totalCount = totalCount;
         this.totalPrice = totalPrice;
+        this.orderRequest = orderRequest;
         this.paymentType = paymentType;
         this.createdAt = createdAt;
     }
