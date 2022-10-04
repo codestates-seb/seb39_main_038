435dc651d6765dc65a4517a17d6db66e8bb69690
김나율 <96483208+kal1777@users.noreply.github.com>
2022-10-05 05:05:35 +0900

    feat: storeType별 목록 불러오기 추가 (#199)
    * feat: review, store CRUD 추가
    
    * feat: review, store CRUD 추가
    
    * feat: Local entity 수정
    
    * feat: Local entity 수정
    
    * fix: review CRUD 수정
    
    * fix: review Mapper 수정
    
    * feat : crud test
    
    * feat: crud test 원복
    
    * feat: review, store, comment CRUD 추가
    
    * feat: storeType별 목록 불러오기 추가

--- src/main/java/com/main_39/Spring/store/repository/StoreRepository.java
+++ src/main/java/com/main_39/Spring/store/repository/StoreRepository.java
@@ -1,6 +1,8 @@
 package com.main_39.Spring.store.repository;
 
 import com.main_39.Spring.store.entity.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +11,9 @@ import java.util.Optional;
 public interface StoreRepository extends JpaRepository<Store, Long> {
     @Query(value = "SELECT * FROM store WHERE store_name = :storeName", nativeQuery = true)
     Optional<Store> findByName(String storeName);
+
+    /**
+     * 푸드트럭 타입별 푸드트럭 목록 불러오기
+     */
+    public Page<Store> findByStoreType(Store.StoreType storeType, PageRequest pageable);
 }
