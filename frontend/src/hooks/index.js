c011e156e5a075694d97ac9be1b1db6ede4ce1a7
YHJ96 <yhj878812@naver.com>
2022-10-04 12:14:54 +0900

    feat: useDetailFoodList 추가 완료

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -2,5 +2,6 @@ import { useModal } from './useModal';
 import { useFoodList } from './useFoodList';
 import { useMyPage } from './useMyPage';
 import { useFoodDetail } from './useFoodDetail';
+import { useDetailFoodList } from './useDetailFoodList';
 
-export { useModal, useFoodList, useMyPage, useFoodDetail };
+export { useModal, useFoodList, useMyPage, useFoodDetail, useDetailFoodList };
