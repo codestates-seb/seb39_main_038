feb4fe123e88a47f969674370e8caba350fffbf1
YHJ96 <yhj878812@naver.com>
2022-10-05 15:04:31 +0900

    feat: usePay 추가 완료

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -3,5 +3,13 @@ import { useFoodList } from './useFoodList';
 import { useMyPage } from './useMyPage';
 import { useFoodDetail } from './useFoodDetail';
 import { useDetailFoodList } from './useDetailFoodList';
+import { usePay } from './usePay';
 
-export { useModal, useFoodList, useMyPage, useFoodDetail, useDetailFoodList };
+export {
+  useModal,
+  useFoodList,
+  useMyPage,
+  useFoodDetail,
+  useDetailFoodList,
+  usePay,
+};
