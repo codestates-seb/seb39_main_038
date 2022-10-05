e286a03980acb47a6e415c5742687edb7c154e43
YHJ96 <yhj878812@naver.com>
2022-10-05 21:57:31 +0900

    feat: foodReview 더미 데이터 구현 완료

---
+++ server/data/foodReview.js
@@ -0,0 +1,29 @@
+const data = [];
+
+for (let i = 0; i < 100; i++) {
+  const reviews = [];
+  for (let i = 0; i < 5; i++) {
+    const obj = {
+      reviewId: i,
+      reviewContent: "내용",
+      reviewImage: null,
+      reviewGrade: 5,
+      createdAt: new Date(),
+      comment: {
+        commentId: i,
+        commentContent: "답장",
+        createdAt: new Date(),
+      },
+    };
+    reviews.push(obj);
+  }
+  data.push({
+    storeId: i,
+    reviews: reviews,
+    totalReview: 100,
+    totalGrade: 4,
+    totalComment: 100,
+  });
+}
+
+module.exports = { data };
