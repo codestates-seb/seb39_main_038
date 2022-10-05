0f1ebc3068b1c799abf851eea2ca6978f76a2d00
김나율 <96483208+kal1777@users.noreply.github.com>
2022-10-06 04:18:36 +0900

    fix: review CRUD 수정 (#218)
    * feat: review, store CRUD 추가
    
    * feat: review, store CRUD 추가
    
    * feat: Local entity 수정
    
    * feat: Local entity 수정
    
    * fix: review CRUD 수정
    
    * fix: review Mapper 수정
    
    * feat : crud test
    
    * feat: crud test 원복
    
    * feat: review, store, comment CRUD 추가
    
    * feat: storeType별 목록 불러오기 추가
    
    * feat: store 중복 예외처리 추가
    
    * fix: store update 수정
    
    * fix: review CRUD 수정
    
    * fix: review CRUD 수정
    
    * feat: github actions test
    
    * feat: comment update 추가
    
    * fix: review get 수정

--- src/main/java/com/main_39/Spring/review/dto/ReviewsResponseDto.java
+++ src/main/java/com/main_39/Spring/review/dto/ReviewsResponseDto.java
@@ -9,17 +9,22 @@ import java.util.List;
 public class ReviewsResponseDto {
     private long storeId;
     private List<ReviewResponseDto> reviews;
+
     private int totalReview;
     private double totalGrade;
     private int totalComment;
 
+    private int totalMenu;
+
+
     @Builder
     public ReviewsResponseDto(Long storeId, int totalReview, double totalGrade, int totalComment,
-                              List<ReviewResponseDto> reviews) {
+                              int totalMenu, List<ReviewResponseDto> reviews) {
         this.storeId = storeId;
         this.reviews = reviews;
         this.totalReview = totalReview;
         this.totalGrade = totalGrade;
         this.totalComment = totalComment;
+        this.totalMenu = totalMenu;
     }
 }
