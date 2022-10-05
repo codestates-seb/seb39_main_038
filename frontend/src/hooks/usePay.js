81862258ce97e3789024812725be24cba3a87384
YHJ96 <yhj878812@naver.com>
2022-10-05 15:02:26 +0900

    feat: usePay 데이터 정제 완료

--- frontend/src/hooks/usePay.js
+++ frontend/src/hooks/usePay.js
@@ -2,32 +2,38 @@
 
 import axios from 'axios';
 import { useRecoilValue } from 'recoil';
+import { useQueryClient } from '@tanstack/react-query';
 import { atoms } from '../store';
+import { randomRange, dateFormat } from '../utils';
 
 const { IMP_KEY } = process.env;
 
 IMP.init(IMP_KEY);
 
-function usePay() {
+function usePay(id) {
+  const queryClinet = useQueryClient();
   const orderList = useRecoilValue(atoms.orderList);
   const orderMenus = orderList.map((item) => ({
     menuId: item.menuId,
     count: item.count,
   }));
 
+  const data = queryClinet.getQueryData(['foodDetail', id]);
+
   const goPay = (orderRequest, paymentType) => {
     IMP.request_pay(
       {
         pg: 'html5_inicis',
         pay_method: 'card',
-        merchant_uid: 'ORD20180131-0000015',
-        name: '노르웨이 회전 의자',
+        merchant_uid: `ORD${dateFormat(new Date(), '')}-${randomRange(
+          0,
+          100000,
+        )}`,
+        name: data?.data.data.storeName,
         amount: 100,
-        buyer_email: 'gildong@gmail.com',
-        buyer_name: '홍길동',
-        buyer_tel: '010-4242-4242',
-        buyer_addr: '서울특별시 강남구 신사동',
-        buyer_postcode: '01181',
+        buyer_tel: data?.data.data.storePhone,
+        buyer_addr: data?.data.data.storeAddress,
+        buyer_postcode: data?.data.data.storeId,
       },
 
       async (rsp) => {
