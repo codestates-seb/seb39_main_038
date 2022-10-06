f8552fc09ddbb68dfbc35b06f5712f53065a2a69
YHJ96 <yhj878812@naver.com>
2022-10-07 02:43:35 +0900

    rename: createMutate 변수 이름 수정

--- frontend/src/hooks/useAnswer.js
+++ frontend/src/hooks/useAnswer.js
@@ -3,18 +3,19 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { API_URI } from '../constants';
 
 const fetchAnswer = async ({ id, value }) => {
+  console.log(id, value);
   const { storeId, reviewId, commentContent } = value;
   await axios.post(API_URI.ANSWER(id), { storeId, reviewId, commentContent });
 };
 
 function useAnswer(id) {
   const queryClient = useQueryClient();
-  const { mutate: fetchCreateAnswer } = useMutation(fetchAnswer, {
+  const { mutate: createMutate } = useMutation(fetchAnswer, {
     onSuccess: () => {
       queryClient.invalidateQueries(['review', id]);
     },
   });
-  return { fetchCreateAnswer };
+  return { createMutate };
 }
 
 export { useAnswer };
