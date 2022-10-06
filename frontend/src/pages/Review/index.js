10a67a1b4e6dee6aa8749c7c873f191a6bb57785
YHJ96 <yhj878812@naver.com>
2022-10-06 15:55:32 +0900

    feat: 페이지 주소 접근 예외 처리

--- frontend/src/pages/Review/index.js
+++ frontend/src/pages/Review/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import AskReview from './AskReview';
 import UpdateReview from './UpdateReview';
 
 function Review() {
+  const navigate = useNavigate();
   const location = useLocation();
+
+  useEffect(() => {
+    if (!location.state) {
+      alert('잘못된 접근 입니다.');
+      navigate('/');
+    }
+  }, [navigate, location.state]);
+
+  if (!location.state) return <div />;
+
   return location.state.type !== 'post' ? (
     <UpdateReview
       reviewId={location.state.reviewId}
