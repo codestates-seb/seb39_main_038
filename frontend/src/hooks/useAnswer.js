318f4730329114c23a1c0b6fc3392fe4f111f645
YHJ96 <yhj878812@naver.com>
2022-10-07 02:57:37 +0900

    comment: 주석 처리 완료

--- frontend/src/hooks/useAnswer.js
+++ frontend/src/hooks/useAnswer.js
@@ -3,17 +3,15 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { API_URI } from '../constants';
 
 const fetchAnswer = async ({ id, value }) => {
-  console.log(id, value);
   const { storeId, reviewId, commentContent } = value;
   await axios.post(API_URI.ANSWER(id), { storeId, reviewId, commentContent });
 };
 
 function useAnswer(id) {
+  console.log('answer', id);
   const queryClient = useQueryClient();
   const { mutate: createMutate } = useMutation(fetchAnswer, {
-    onSuccess: () => {
-      queryClient.invalidateQueries(['review', id]);
-    },
+    onSuccess: () => queryClient.invalidateQueries(['review', id]),
   });
   return { createMutate };
 }
