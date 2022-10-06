878525a4671e0d3e88cfe358b7371383eab09ee2
ds02168 <1995musso@gmail.com>
2022-10-07 02:38:12 +0900

    fix : 리뷰 닉네임 출력 수정

--- src/main/java/com/main_39/Spring/member/entity/Local.java
+++ src/main/java/com/main_39/Spring/member/entity/Local.java
@@ -27,7 +27,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @Entity
-public class Local implements Users{
+public class Local{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long localId;
