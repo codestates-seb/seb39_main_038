35db40f9145103c3d156afa2fe5a7de09d269062
YHJ96 <yhj878812@naver.com>
2022-10-06 15:44:53 +0900

    feat: 로그인 컴포넌트 접근 권한 제한

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { atoms } from '../../store';
 import LoginNav from './LoginNav';
 import ProviderLogin from './ProviderLogin';
 import ConsumerLogin from './ConsumerLogin';
 
 function Login() {
+  const navigate = useNavigate();
   const location = useLocation();
   const toRegister = !location.state;
   const [isChecked, setIsChecked] = useState(toRegister);
+  const isLogin = useRecoilValue(atoms.isLogin);
+
+  useEffect(() => {
+    if (isLogin.state) {
+      alert('접근할 수 없습니다.');
+      navigate('/');
+    }
+  }, [isLogin.state, navigate]);
+
   return (
     <>
       <LoginNav isChecked={isChecked} setIsChecked={setIsChecked} />
