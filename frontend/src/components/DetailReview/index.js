2717a424ef8312686902be1440f0398f00e6cd0a
YHJ96 <yhj878812@naver.com>
2022-10-07 02:10:10 +0900

    feat: 답변 기능 구현중

--- frontend/src/components/DetailReview/index.js
+++ frontend/src/components/DetailReview/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   ReviewContainer,
@@ -12,41 +12,28 @@ import {
   ButtonWrapper,
   Button,
   Image,
+  Editor,
+  EditorWrapper,
+  Answer,
 } from './styles';
-import { useReview } from '../../hooks';
+import { useReview, useAnswer } from '../../hooks';
 import { ROUTE } from '../../constants';
 import { dateFormat } from '../../utils';
 
-/* 답장 기능 주석 처리
-<EditorWrapper>
-  <Editor placeholder="클린리뷰 특성상 재생성도 불가능하며 수정도 불가능합니다. 신중하게 작성해주세요." />
-  <Button>전송</Button>
-</EditorWrap
-<Answer>
-  <Header>
-    <TextWrapper>
-      <Text as="h1">사장님</Text>
-      <Text size={12} color="#999999">
-        2022-09-21
-      </Text>
-    </TextWrapper>
-    <Button>삭제</Button>
-  </Header>
-  <Text size={14} color="#666666">
-    안녕하세용~~~~ 감사합니다.
-  </Text>
-</Answer>
-*/
-
 function DetailReview({ storeId }) {
   const navigate = useNavigate();
+  const [toggle, setToggle] = useState(false);
+  const [text, setText] = useState('');
   const { data, deleteMutate } = useReview(storeId);
+  const { fetchCreateAnswer } = useAnswer(storeId);
 
   const createStar = (n) => {
     let stars = '';
     for (let i = 0; i < parseInt(n, 10); i += 1) stars += '★';
     return stars;
   };
+
+  const handleToggle = () => setToggle(!toggle);
 
   const goUpdate = (sid, rid) => () =>
     navigate(`/${ROUTE.REVIEW.PATH}`, {
@@ -57,6 +44,14 @@ function DetailReview({ storeId }) {
     const isCheck = window.confirm('정말 지우시겠습니까?');
     if (isCheck) return deleteMutate({ sid, rid });
     return null;
+  };
+
+  const goAnswer = (reviewId, commentContent) => () => {
+    setToggle(!toggle);
+    fetchCreateAnswer({
+      id: storeId,
+      value: { storeId, reviewId, commentContent },
+    });
   };
 
   const createComment = () => {
@@ -76,6 +71,7 @@ function DetailReview({ storeId }) {
             <ButtonWrapper>
               <Button onClick={goUpdate(storeId, item.reviewId)}>수정</Button>
               <Button onClick={goDelete(storeId, item.reviewId)}>삭제</Button>
+              <Button onClick={handleToggle}>답변</Button>
             </ButtonWrapper>
           </Header>
           <TextWrapper>
@@ -92,6 +88,29 @@ function DetailReview({ storeId }) {
           <Text size={14} color="#666666">
             {item.reviewContent}
           </Text>
+          {toggle ? (
+            <EditorWrapper>
+              <Editor
+                onChange={(e) => setText(e.target.value)}
+                placeholder="클린리뷰 특성상 재생성도 불가능하며 수정도 불가능합니다. 신중하게 작성해주세요."
+              />
+              <Button onClick={goAnswer(item.reviewId, text)}>전송</Button>
+            </EditorWrapper>
+          ) : null}
+          <Answer>
+            <Header>
+              <TextWrapper>
+                <Text as="h1">사장님</Text>
+                <Text size={12} color="#999999">
+                  {dateFormat(new Date(item.comment.createdAt), '-')}
+                </Text>
+              </TextWrapper>
+              <Button>삭제</Button>
+            </Header>
+            <Text size={14} color="#666666">
+              {item.comment.commentContent}
+            </Text>
+          </Answer>
         </Comment>
       );
     });
