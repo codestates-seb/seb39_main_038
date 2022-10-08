a09959f8c82fa7e6130119a26b4e743f6c71a1c9
kal1777 <kimnayul17@gmail.com>
2022-10-09 01:59:25 +0900

    fix: deleteReview 수정

--- src/main/java/com/main_39/Spring/review/controller/ReviewController.java
+++ src/main/java/com/main_39/Spring/review/controller/ReviewController.java
@@ -169,8 +169,9 @@ public class ReviewController {
     public ResponseEntity deleteReview(@PathVariable("store-id") long storeId,
                                        @PathVariable("review-id") @Positive long reviewId) {
         System.out.println("#delete Review");
-        Store store = storeService.findStore(storeId);
-        reviewService.deleteReview(store, reviewId);  // 1009 수정
+//        Store store = storeService.findStore(storeId);
+//        reviewService.deleteReview(store, reviewId);  // 1009 수정
+        reviewService.deleteReview(storeId, reviewId);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
