845fc9dbc4f95beca88df8cb72c83bed269ebc68
kal1777 <kimnayul17@gmail.com>
2022-10-09 04:16:39 +0900

    fix: review 수정

--- src/main/java/com/main_39/Spring/review/dto/ReviewPatchDto.java
+++ src/main/java/com/main_39/Spring/review/dto/ReviewPatchDto.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ReviewPatchDto {
-    private long storeId;
     private long reviewId;
     private String reviewContent;
     private String reviewImage;
