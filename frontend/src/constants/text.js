26ea16a325cd5814180878604109bd183bdf2255
YHJ96 <yhj878812@naver.com>
2022-10-10 20:48:09 +0900

    feat: PWINQUIRY & PWCHANGE & NOTFOUND 프로퍼티 추가 완료

--- frontend/src/constants/text.js
+++ frontend/src/constants/text.js
@@ -10,6 +10,22 @@ const TEXT = {
     CONSUMER_LOGIN: '일반 회원',
     PROVIDER_LOGIN: '사업자 회원',
   },
+
+  PWINQUIRY: {
+    POST_EMAIL: '입력하신 이메일 주소로 비밀번호 재설정 메일이 발송됩니다.',
+    IMPOSSIBLE: '카카오 소셜 회원은 비밀번호 찾기가 불가능합니다.',
+    OPEN_MODAL: '모달창 열기',
+  },
+
+  PWCHANGE: {
+    PASSWORD_FINDING_COMPLETE: '비밀번호 변경 완료',
+  },
+
+  NOTFOUND: {
+    PAGE_NOTFOUND: '찾을 수 없는 페이지 입니다.',
+    WRONG_PATH: '잘못된 경로를 사용하셨어요 :)',
+    GO_HOME: '홈으로 이동',
+  },
 };
 
 export { TEXT };
