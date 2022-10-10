80f1e9ce83110efeb666aebc29926e7dfad676ae
kal1777 <kimnayul17@gmail.com>
2022-10-10 17:08:50 +0900

    fix: review, menu cascade 추가

--- src/main/java/com/main_39/Spring/menu/entity/Menu.java
+++ src/main/java/com/main_39/Spring/menu/entity/Menu.java
@@ -6,7 +6,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/com/main_39/Spring/review/entity/Review.java
+++ src/main/java/com/main_39/Spring/review/entity/Review.java
@@ -9,7 +9,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
 
 @Getter
 @Setter
