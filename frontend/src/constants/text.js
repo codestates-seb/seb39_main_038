e3de5566143e3587509c376f1979fc92745d0677
YHJ96 <yhj878812@naver.com>
2022-10-10 21:33:00 +0900

    feat: ORDER 프로퍼티 추가 완료

--- frontend/src/constants/text.js
+++ frontend/src/constants/text.js
@@ -36,6 +36,21 @@ const TEXT = {
     MENU: (text = '') => `메뉴 ${text}`,
     CLEAN_REVIWE: (text = '') => `클린리뷰 ${text}`,
   },
+
+  ORDER: {
+    PAYMENT: '결제하기',
+    REQUEST: '주문시 요청사항',
+    MAX_LENGTH: '최대 100자까지 입력 가능합니다.',
+    METHOD_OF_PAYMENT: '결제수단 선택',
+    CASH_PAYMENT_TOOLTIP:
+      '현금결제를 원하시는 경우 직접 푸드트럭 앞에서 현금을 지불할 수 있어요!',
+    CARD_PAYMENT: '신용카드',
+    CASH_PAYMENT: '현금',
+    METHOD_OF_DISCOUNT: '할인방법 선택',
+    APPLY: '적용',
+    MILEAGE: (text = 0) => `마일리지: ${text} 원`,
+    LENGHT_CHECK: (text = 100) => `100 / ${text}`,
+  },
 };
 
 export { TEXT };
