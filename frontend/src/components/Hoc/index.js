e3f57f0512bf563f2e268ec345b01c7f9ebf33e5
Jaewon Kim <99641988+Mark1237200@users.noreply.github.com>
2022-10-11 15:04:19 +0900

    feat: 이미지 삽입 기능 추가 & CSS (#340)
    * feat: withAuth, MyPage naviate to trucksetting 기능 추가
    
    * feat: 가게설정 이미지 삽입 기능 추가  &  CSS
    
    * Update index.js
    
    fix: error found  fixed
    
    Co-authored-by: Mark1237200 <“0915kimjw@gmail.com”>

--- frontend/src/components/Hoc/index.js
+++ frontend/src/components/Hoc/index.js
@@ -1,18 +1,28 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { useNavigate, useParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import { useEffect } from 'react';
 import { ROUTE } from '../../constants';
+import { atoms } from '../../store';
 
 // eslint-disable-next-line react/function-component-definition
 const withAuth = (Component) => (props) => {
   const { id } = useParams();
 
   const navigate = useNavigate();
+  const { localId } = useRecoilValue(atoms.loginInfo);
+  const { state } = useRecoilValue(atoms.isLogin);
 
   useEffect(() => {
-    if (Number(id) !== JSON.parse(sessionStorage.getItem('storeId')).localId) {
-      alert('로그인 정보와 다른 정보입니다.');
+    console.log(id !== localId, 'url:', id, 'local:', localId);
+    if (state) {
+      if (Number(id) !== localId) {
+        alert('로그인 정보와 다른 정보입니다.');
 
+        navigate(ROUTE.HOME.PATH);
+      }
+    } else {
+      alert('로그인을 먼저 하셔야 합니다.');
       navigate(ROUTE.HOME.PATH);
     }
   }, [id, navigate]);
