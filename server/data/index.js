a398633f72965b878bff113809b41f192d415d90
YHJ96 <yhj878812@naver.com>
2022-10-15 21:45:47 +0900

    rename: backend 폴더로 파일 이동 완료

--- server/data/index.js
+++
@@ -1,5 +0,0 @@
-const { data: foodListData } = require("./foodList");
-const { createFoods } = require("./menuList");
-const { data: foodReviewData } = require("./foodReview");
-
-module.exports = { foodListData, foodReviewData, createFoods };
--- server/utils/index.js
+++
@@ -1,4 +0,0 @@
-const randomRange = (start, end) =>
-  Math.floor(Math.random() * (end - start) + start);
-
-module.exports = { randomRange };
